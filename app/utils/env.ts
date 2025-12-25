703d4939771380a1bd7bef0057283c8ea6e11aec
risingprincecapital <risingprincecapital@gmail.com>
2025-12-25 16:31:53 +0530

    key variable update

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: process.env.NEXT_PUBLIC_API_URL || '',
-    GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
+    API_URL: 'https://kdsxegdbgc.us-east-1.awsapprunner.com/api',//process.env.NEXT_PUBLIC_API_URL || '',
+    GOOGLE_CLIENT_ID: '496547749564-ent4sknmtr09obbtobavfh90bdkuaibl.apps.googleusercontent.com'//process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
 };
