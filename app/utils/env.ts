876e6ecba62b19fe424ee14e67d80dcf3623fed6
risingprincecapital <risingprincecapital@gmail.com>
2025-12-29 18:00:58 +0530

    fix for google auth login

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: 'https://kdsxegdbgc.us-east-1.awsapprunner.com/api',//process.env.NEXT_PUBLIC_API_URL || '',
-    GOOGLE_CLIENT_ID: '496547749564-ent4sknmtr09obbtobavfh90bdkuaibl.apps.googleusercontent.com'//process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
+    API_URL: 'https://kdsxegdbgc.us-east-1.awsapprunner.com/api', // process.env.NEXT_PUBLIC_API_URL || ''
+    GOOGLE_CLIENT_ID: '1028730797656-e5rdfdsigampr9if0bpilt2h5bi2n1go.apps.googleusercontent.com'//process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
 };
